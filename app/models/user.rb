5669a628da6f15b35a77ed72133c615dff1b5863
Michael Reeves <mreeves68@yahoo.com>
2014-07-18 15:12:26 -0300

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ActiveRecord::Base
   # not all database adapters use case sensitive indexes, 
   # so downcase the email in the before_save callback
-  before_save { self.email = email.downcase }
+  #before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   
   # include password hashing and authentication features
   has_secure_password
   
   
   # validators
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   validates( :name, { presence: true, length: { maximum: 50 } } )
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
