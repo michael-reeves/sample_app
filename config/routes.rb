9e797f6b67deb81e8c42c6cf4ecbba8860295c0c
Michael Reeves <mreeves68@yahoo.com>
2014-07-25 12:00:09 -0300

    Add profile page and user signup with errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-  
+  # map all users actions needed for RESTful design
+  # and a number of named routes
+  resources :users
+
   root 'static_pages#home'
   match '/signup',  to: 'users#new',             via: 'get'
   match '/help',    to: 'static_pages#help',     via: 'get'
